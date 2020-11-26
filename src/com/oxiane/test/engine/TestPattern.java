6ea3add786b74dcb2c97b9049976af0f7083158e
aandria <aandria@oxiane.com>
2020-11-26 16:57:39 +0100

    refractoring debut

--- src/com/oxiane/test/engine/TestPattern.java
+++ src/com/oxiane/test/engine/TestPattern.java
@@ -4,7 +4,9 @@ package com.oxiane.test.engine;
 import java.util.Timer;
 
 import com.oxiane.test.controleur.TestControler;
+import com.oxiane.test.controleur.TestControler.MyTimerTaskControler;
 import com.oxiane.test.modele.TestModel;
+import com.oxiane.test.modele.TestModel.MyTimerTaskModel;
 import com.oxiane.test.vue.TestView;
 
 public class TestPattern {
@@ -13,44 +15,65 @@ public class TestPattern {
 	private static TestView testview = new TestView();
 	private static TestControler testcontroler = new TestControler(testmodel);
 	
+	public Timer CreateTimer() {
+		Timer timer= new Timer(true);
+		return timer;
+	}
+	
+	public void CreateSchedulModel(Timer timer, MyTimerTaskModel timerTaskModel) {
+		timer.scheduleAtFixedRate(timerTaskModel, 20, 10);
+		System.out.println("TimerTaskModel started");
+	}
+	
+	public void CreateSchedulControler(Timer timer, MyTimerTaskControler timerTaskControler) {
+		timer.scheduleAtFixedRate(timerTaskControler, 0, 20);
+		System.out.println("TimerTaskControler started");
+	}
+	
+	public void TimerCancel(Timer timer) {
+		timer.cancel();
+		System.out.println(timer + " cancelled ");
+	}
+	
+	public void ThreadSleep(long delay) {
+		try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+	}
+	
+	public void CancelTimerTask(TestPattern pattern, Timer timer1, Timer timer2) {
+
+		pattern.ThreadSleep(120000);
+		
+        pattern.TimerCancel(timer1);
+        pattern.TimerCancel(timer2);
+        
+        pattern.ThreadSleep(30000);
+	}
+	
+	public void launchProgram(TestPattern pattern) {
+		
+		TestModel.MyTimerTaskModel timerTaskModel = testmodel.new MyTimerTaskModel();
+		TestControler.MyTimerTaskControler timerTaskControler = testcontroler.new MyTimerTaskControler();
+		
+		Timer timerModel = pattern.CreateTimer();
+		Timer timerControler = pattern.CreateTimer();
+		
+		pattern.CreateSchedulControler(timerControler, timerTaskControler);
+		pattern.CreateSchedulModel(timerModel, timerTaskModel);
+		
+		pattern.CancelTimerTask(pattern, timerModel, timerControler);
+	}
+//
 	
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		testmodel.addObserver(testview);
-		
-		TestModel.MyTimerTaskModel timerTaskModel = testmodel.new MyTimerTaskModel();
-		TestControler.MyTimerTaskControler timerTaskControler = testcontroler.new MyTimerTaskControler();
-		
-        //running timer task as daemon thread
-        Timer timerModel = new Timer(true);
-        Timer timerControler = new Timer(true);
-        
-        timerModel.scheduleAtFixedRate(timerTaskModel, 20, 10);
-        System.out.println("TimerTaskModel started");
-        
-        timerControler.scheduleAtFixedRate(timerTaskControler, 0, 20);
-        System.out.println("TimerTaskControler started");
-        
-        //cancel after sometime
-        try {
-            Thread.sleep(120000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        timerModel.cancel();
-        System.out.println("TimerTaskModel cancelled");
-        
-        timerControler.cancel();
-        System.out.println("TimerTaskControler cancelled");
-        
-        
-        try {
-            Thread.sleep(30000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+		TestPattern pattern = new TestPattern();
+		pattern.launchProgram(pattern);
 	}
-
 }
