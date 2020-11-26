f8fe188037330a5961b10d7b249e8bc0f1938a69
aandria <aandria@oxiane.com>
2020-11-26 16:09:57 +0100

    modif

--- src/com/oxiane/test/controleur/TestControler.java
+++ src/com/oxiane/test/controleur/TestControler.java
@@ -1,5 +1,7 @@
 package com.oxiane.test.controleur;
 
+import java.util.TimerTask;
+
 import com.oxiane.test.modele.TestModel;
 
 public class TestControler {
@@ -7,5 +9,23 @@ public class TestControler {
 	
 	public TestControler(TestModel testmodele) {
 		this.testmodele=testmodele;
+	}
+	
+	public class MyTimerTaskControler extends TimerTask {
+
+	    @Override
+	    public void run() {
+	        completeTask();
+	    }
+
+	    private void completeTask() {
+	        try {
+	            //assuming it takes 5 secs to complete the task
+	            Thread.sleep(5000);
+	            testmodele.increment();
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
 }
--- src/com/oxiane/test/modele/TestModel.java
+++ src/com/oxiane/test/modele/TestModel.java
@@ -1,6 +1,7 @@
 package com.oxiane.test.modele;
 
 import java.util.Observable;
+import java.util.TimerTask;
 
 @SuppressWarnings("deprecation")
 public class TestModel extends Observable{
@@ -21,5 +22,28 @@ public class TestModel extends Observable{
 	
 	public int getVariable() {
 		return variable;
+	}
+	
+	public void notifyObservers() {
+		setChanged();
+		notify();
+	}
+	
+	public class MyTimerTaskModel extends TimerTask {
+
+	    @Override
+	    public void run() {
+	        completeTask();
+	    }
+
+	    private void completeTask() {
+	        try {
+	            //assuming it takes 5 secs to complete the task
+	            Thread.sleep(5000);
+	            notifyObservers();
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
 }
